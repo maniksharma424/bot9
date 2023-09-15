02e129e84c25d0e57faf230ffaa92d2df7501a82
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-09-15 20:03:01 +0530

    style:fix

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -10,9 +10,9 @@ const Inbox = ({ messages }) => {
     <div className="border-r-[0.5px] w-full  lg:w-1/4 h-full flex flex-col ">
       <div
         id="search-box"
-        className="w-full h-[65px] border-[0.5px] flex justify-between items-center  px-5  text-[25px] font-[400] text-black"
+        className="w-full h-[65px] border-[0.5px] flex justify-between items-center  sm:px-5  pr-4  text-[25px] font-[400] text-black"
       >
-        <div className="block sm:hidden text-black ">
+        <div className=" sm:hidden text-black w-1/6 flex justify-center ">
           <GiHamburgerMenu />
         </div>
         <p>inbox</p>
